872dc3473ebeb0cea8bd4fe6d779a519236ba851
Harshit <harshit@Admins-MacBook-Pro.local>
2020-10-21 23:19:44 +0530

    Updates

---
+++ precache-manifest.510dafec98ee496b97486413f5415f85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "babd8dcfaf4b6c380adf0e28716f0e8a",
+    "url": "/Photo-Feed/index.html"
+  },
+  {
+    "revision": "25e41dd8322850aac7da",
+    "url": "/Photo-Feed/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "dc94862240f0fd0ae9ec",
+    "url": "/Photo-Feed/static/css/main.36d06101.chunk.css"
+  },
+  {
+    "revision": "25e41dd8322850aac7da",
+    "url": "/Photo-Feed/static/js/2.6017baa4.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Photo-Feed/static/js/2.6017baa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc94862240f0fd0ae9ec",
+    "url": "/Photo-Feed/static/js/main.e9b6b90e.chunk.js"
+  },
+  {
+    "revision": "77aab87ae6353ba593e8",
+    "url": "/Photo-Feed/static/js/runtime-main.1784db22.js"
+  }
+]);
